7c21f0be2ea72ec257951160b6987a1b727cafb9
rusakot <barabulka-01@mail.ru>
2025-10-01 10:39:49 +0500

    new arch: vite+react+ts

--- script.js
+++
@@ -1,2 +0,0 @@
-const currentYear = new Date().getFullYear(); //Автозамена года
-document.getElementById('footer__par').textContent = currentYear + ' ' + document.getElementById('footer__par').textContent;
